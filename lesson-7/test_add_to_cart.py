3d1508ef7cdec7a7da44148a73a3e784d7bb7ae8
Anastasiya Gayvoronskaya <a.gayvoronskaya@office.ngs.ru>
2018-11-22 00:22:52 +0700

    Added test for cart with wait

--- lesson-7/test_add_to_cart.py
+++ lesson-7/test_add_to_cart.py
@@ -1,16 +1,16 @@
 import pytest
-import time
 
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
 
 @pytest.fixture()
 def set_driver(request):
     driver = webdriver.Chrome()
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(0.5)
     request.addfinalizer(driver.quit)
     return driver
 
@@ -24,22 +24,21 @@ def test_add_product_to_cart(set_driver):
     cart = driver.find_element_by_xpath('//a[text()="Checkout »"]')
     cart.click()
 
-    count_products_in_cart = driver.find_elements_by_xpath('//td[@class="item"]')
+    count_products_in_cart = driver.find_elements_by_xpath(
+        '//td[@class="item"]')
 
     i =1
     while i <= len(count_products_in_cart):
-        remove_button = driver.find_element_by_xpath('//button[@name="remove_cart_item"]')
+        product_name = driver.find_element_by_xpath('//li//strong').text
+        table_product_name = driver.find_element_by_xpath(
+            '//td[text() = "%s"]' % product_name)
+
+        remove_button = driver.find_element_by_xpath(
+            '//button[@name="remove_cart_item"]')
         remove_button.click()
-        WebDriverWait(driver, 10).until(
-            lambda d, : d.find_elements_by_xpath(
-                '//td[@class="item"]'))
-        count_products_in_cart = driver.find_elements_by_xpath(
-            '//td[@class="item"]')
-        print(len(count_products_in_cart))
+
+        WebDriverWait(driver, 10).until(EC.staleness_of(table_product_name))
         i +=1
-
-
-    time.sleep(10)
 
 
 def add_to_cart(driver, items):
